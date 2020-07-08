447b60815c268ec7bfeeb001c3c92aa7f58e69ee
子墨 <461009747@qq.com>
2020-07-08 22:08:19 +0800

    自动签到->通用

--- README.md
+++ README.md
@@ -1,27 +1,37 @@
 # auto-sign
 
-受人之托，写的今日校园自动签到脚本，目前适用于下表：
+受人之托，写的今日校园自动签到脚本，支持图片，定位，额外选项等，基本上应该算是通用吧，目前适用于下表：
 
 <table>
     <tr>
         <th>学校名称</th>
         <th>学校英文简称</th>
         <th>加入今日校园的方式</th>
+        <th>签到说明</th>
     </tr>
     <tr>
         <td>西南大学</td>
         <td>swu</td>
         <td>NOTCLOUD</td>
+        <td>定位+额外选项</td>
     </tr>
     <tr>
         <td>长江师范学院</td>
         <td>yznu</td>
         <td>NOTCLOUD</td>
+        <td>定位+额外选项</td>
     </tr>
     <tr>
         <td>安阳工学院</td>
         <td>ayit</td>
         <td>CLOUD</td>
+        <td>定位+照片</td>
+    </tr>
+    <tr>
+        <td>新乡医学院</td>
+        <td>xxmu</td>
+        <td>CLOUD</td>
+        <td>定位+照片</td>
     </tr>
 </table>
 
@@ -38,7 +48,11 @@
     ```shell script
     git clone https://github.com/ZimoLoveShuang/auto-sign.git
     ```
-2. 修改`index_xxxx.py`中的对应配置`xxxx`对应学校英文简称，如西南大学的简称是`swu`，对应的脚本就是`index_swu.py`，以此类推
+2. 修改`config_xxxx.yml`中的对应配置，`xxxx`对应学校英文简称，如西南大学的简称是`swu`，对应的配置就是`config_swu.yml`，以此类推，修改`index.py`23行读取的配置文件参数
+    ```python
+    # 全局配置
+    config = getYmlConfig(yaml_file='config.yml')
+    ```
 3. 浏览器访问[腾讯云函数控制台](https://console.cloud.tencent.com/scf/index?rid=1)
 4. 登陆认证之后，点击左侧菜单中的函数服务，然后新建一个云函数，名称随意，运行环境选择`python3.6`，创建方式选空白函数，然后点击下一步
 5. 提交方法选择本地上传文件夹，选择下载配置好的仓库文件夹，点击下面的高级设置，设置超时时间为60秒，然后点击完成
@@ -47,6 +61,9 @@
     0 10 7,19 * * * *
     ```
 7. enjoy it!!!
+8. 默认配置适用于`新乡医学院`
+
+#### 如果你不会配置表单组默认选项配置，请先配置好user信息之后本地执行generate.py然后将分割线下的内容复制到配置文件中对应位置
 
 #### 如遇到依赖问题，请去[`auto-sumit`](https://github.com/ZimoLoveShuang/auto-submit)项目下载`dependency.zip`，然后参考`auto-submit`项目的说明将函数依赖层添加到腾讯云函数
 
@@ -56,6 +73,6 @@
 
 # 其他
 
-1. 项目依赖于[模拟登陆 金智教务统一登陆系统 的API](https://github.com/ZimoLoveShuang/wisedu-unified-login-api)
+1. 项目依赖于我的开源项目[模拟登陆 金智教务统一登陆系统 的API](https://github.com/ZimoLoveShuang/wisedu-unified-login-api)
 2. `Cpdaily-Extension`是逆向今日校园APK后得到的，本质上就是对一个json对象进行了des加密，然后编码为了Base64字符串，加密解密实现可以参考[DESHelper](https://github.com/ZimoLoveShuang/yibinu-score-crawler/blob/master/src/main/java/wiki/zimo/scorecrawler/helper/DESHelper.java)
-3. 也欢迎其他学校学子在此提交适用于自己学校的脚本，命名规则为`index_xxxx.py`，`xxxx`为学校简称
+3. 也欢迎其他学校学子在此提交适用于自己学校的配置，命名规则为`config_xxxx.yml`，`xxxx`为学校英文简称
--- index.py
+++ index.py
@@ -7,41 +7,20 @@ from pyDes import des, CBC, PAD_PKCS5
 from datetime import datetime, timedelta, timezone
 from urllib.parse import urlparse
 import oss2
+import yaml
 
-################### 配置 #####################
-# 学校
-school = '安阳工学院'
-# 学号
-username = 'text'
-# 密码
-password = 'text'
-# 邮箱，用于接收签到成功之后的消息
-email = '461009747@qq.com'
-# 位置
-position = '河南省驻马店市平舆县泰和路'
-# 经度
-lon = '114.639377'
-# 纬度
-lat = '32.958833'
-# 原因
-abnormalReason = ' 1.体温：36.5；2.健康状况：良好 3.在家'
-# 图片
-photo = 'default.png'
-########################################
 
-# Cpdaily-Extension
-extension = {
-    "lon": lon,
-    "model": "OPPO R11 Plus",
-    "appVersion": "8.1.14",
-    "systemVersion": "4.4.4",
-    "userId": username,
-    "systemName": "android",
-    "lat": lat,
-    "deviceId": str(uuid.uuid1())
-}
+# 读取yml配置
+def getYmlConfig(yaml_file='config.yml'):
+    file = open(yaml_file, 'r', encoding="utf-8")
+    file_data = file.read()
+    file.close()
+    config = yaml.load(file_data, Loader=yaml.FullLoader)
+    return dict(config)
 
-session = requests.session()
+
+# 全局配置
+config = getYmlConfig()
 
 
 # 获取当前utc时间，并格式化为北京时间
@@ -58,14 +37,15 @@ def log(content):
 
 
 # 获取今日校园api
-def getCpdailyApis():
+def getCpdailyApis(user):
     apis = {}
+    user = user['user']
     schools = requests.get(url='https://www.cpdaily.com/v6/config/guest/tenant/list').json()['data']
     flag = True
     for one in schools:
-        if one['name'] == school:
+        if one['name'] == user['school']:
             if one['joinType'] == 'NONE':
-                log(school + ' 未加入今日校园或者学校全称错误')
+                log(user['school'] + ' 未加入今日校园或者学校全称错误')
                 exit(-1)
             flag = False
             params = {
@@ -91,31 +71,30 @@ def getCpdailyApis():
                 apis['host'] = host
             break
     if flag:
-        log(school + ' 未加入今日校园或者学校全称错误')
+        log(user['school'] + ' 未加入今日校园或者学校全称错误')
         exit(-1)
     log(apis)
     return apis
 
 
-apis = getCpdailyApis()
-
-
-# 登陆并获取cookies
-def getCookies():
+# 登陆并获取session
+def getSession(user, apis):
+    user = user['user']
     params = {
         # 'login_url': 'http://authserverxg.swu.edu.cn/authserver/login?service=https://swu.cpdaily.com/wec-counselor-sign-apps/stu/sign/getStuSignInfosInOneDay',
         'login_url': apis['login-url'],
         'needcaptcha_url': '',
         'captcha_url': '',
-        'username': username,
-        'password': password
+        'username': user['username'],
+        'password': user['password']
     }
 
     cookies = {}
     # 借助上一个项目开放出来的登陆API，模拟登陆
-    res = requests.post('http://www.zimo.wiki:8080/wisedu-unified-login-api-v1.0/api/login', params)
+    res = requests.post(url=config['login']['api'], data=params)
     # cookieStr可以使用手动抓包获取到的cookie，有效期暂时未知，请自己测试
     cookieStr = str(res.json()['cookies'])
+    log(cookieStr)
     if cookieStr == 'None':
         log(res.json())
         exit(-1)
@@ -125,11 +104,13 @@ def getCookies():
     for line in cookieStr.split(';'):
         name, value = line.strip().split('=', 1)
         cookies[name] = value
+    session = requests.session()
     session.cookies = requests.utils.cookiejar_from_dict(cookies, cookiejar=None, overwrite=True)
+    return session
 
 
 # 获取最新未签到任务
-def getUnSignedTasks():
+def getUnSignedTasks(session, apis):
     headers = {
         'Accept': 'application/json, text/plain, */*',
         'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.97 Safari/537.36',
@@ -149,6 +130,7 @@ def getUnSignedTasks():
     if len(res.json()['datas']['unSignedTasks']) < 1:
         log('当前没有未签到任务')
         exit(-1)
+    log(res.json())
     latestTask = res.json()['datas']['unSignedTasks'][0]
     return {
         'signInstanceWid': latestTask['signInstanceWid'],
@@ -156,8 +138,8 @@ def getUnSignedTasks():
     }
 
 
-# 获取签到任务详情，并构造表单
-def getDetailTask(params):
+# 获取签到任务详情
+def getDetailTask(session, params, apis):
     headers = {
         'Accept': 'application/json, text/plain, */*',
         'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.97 Safari/537.36',
@@ -169,21 +151,50 @@ def getDetailTask(params):
     res = session.post(
         url='https://{host}/wec-counselor-sign-apps/stu/sign/detailSignInstance'.format(host=apis['host']),
         headers=headers, data=json.dumps(params))
-    submitForm = {
-        "signInstanceWid": params['signInstanceWid'],
-        "longitude": lon,
-        "latitude": lat,
-        "isMalposition": 1,
-        "abnormalReason": abnormalReason,
-        "signPhotoUrl": getPictureUrl(uploadPicture(photo)),
-        "position": position,
-        "isNeedExtra": 0
-    }
-    return submitForm
+    data = res.json()['datas']
+    return data
+
+
+# 填充表单
+def fillForm(task, session, user, apis):
+    user = user['user']
+    form = {}
+    if task['isPhoto'] == 1:
+        fileName = uploadPicture(session, user['photo'], apis)
+        form['signPhotoUrl'] = getPictureUrl(session, fileName, apis)
+    else:
+        form['signPhotoUrl'] = ''
+    if task['isNeedExtra'] == 1:
+        extraFields = task['extraField']
+        defaults = config['cpdaily']['defaults']
+        extraFieldItemValues = []
+        for i in range(0, len(extraFields)):
+            default = defaults[i]['default']
+            extraField = extraFields[i]
+            if default['title'] != extraField['title']:
+                log('第%d个默认配置项错误，请检查' % (i + 1))
+                exit(-1)
+            extraFieldItems = extraField['extraFieldItems']
+            for extraFieldItem in extraFieldItems:
+                if extraFieldItem['content'] == default['value']:
+                    extraFieldItemValue = {'extraFieldItemValue': default['value'],
+                                           'extraFieldItemWid': extraFieldItem['wid']}
+                    extraFieldItemValues.append(extraFieldItemValue)
+        # log(extraFieldItemValues)
+        # 处理带附加选项的签到
+        form['extraFieldItems'] = extraFieldItemValues
+    # form['signInstanceWid'] = params['signInstanceWid']
+    form['signInstanceWid'] = task['signInstanceWid']
+    form['longitude'] = user['lon']
+    form['latitude'] = user['lat']
+    form['isMalposition'] = task['isMalposition']
+    form['abnormalReason'] = user['abnormalReason']
+    form['position'] = user['address']
+    return form
 
 
 # 上传图片到阿里云oss
-def uploadPicture(image):
+def uploadPicture(session, image, apis):
     url = 'https://{host}/wec-counselor-sign-apps/stu/sign/getStsAccess'.format(host=apis['host'])
     res = session.post(url=url, headers={'content-type': 'application/json'}, data=json.dumps({}))
     datas = res.json().get('datas')
@@ -203,7 +214,7 @@ def uploadPicture(image):
 
 
 # 获取图片上传位置
-def getPictureUrl(fileName):
+def getPictureUrl(session, fileName, apis):
     url = 'https://{host}/wec-counselor-sign-apps/stu/sign/previewAttachment'.format(host=apis['host'])
     data = {
         'ossKey': fileName
@@ -223,7 +234,20 @@ def DESEncrypt(s, key='ST83=@XV'):
 
 
 # 提交签到任务
-def submit(form):
+def submitForm(session, user, form, apis):
+    user = user['user']
+    # Cpdaily-Extension
+    extension = {
+        "lon": user['lon'],
+        "model": "OPPO R11 Plus",
+        "appVersion": "8.1.14",
+        "systemVersion": "4.4.4",
+        "userId": user['username'],
+        "systemName": "android",
+        "lat": user['lat'],
+        "deviceId": str(uuid.uuid1())
+    }
+
     headers = {
         # 'tenantId': '1019318364515869',
         'User-Agent': 'Mozilla/5.0 (Linux; Android 4.4.4; OPPO R11 Plus Build/KTU84P) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/33.0.0.0 Safari/537.36 okhttp/3.12.4',
@@ -239,13 +263,13 @@ def submit(form):
                        headers=headers, data=json.dumps(form))
     message = res.json()['message']
     if message == 'SUCCESS':
-        sendMessage('自动签到成功')
+        sendMessage('自动签到成功', user['email'])
     else:
-        sendMessage('自动签到失败，原因是：' + message)
+        sendMessage('自动签到失败，原因是：' + message, user['email'])
 
 
 # 发送邮件通知
-def sendMessage(msg):
+def sendMessage(msg, email):
     send = email
     if send != '':
         log('正在发送邮件通知。。。')
@@ -261,10 +285,14 @@ def sendMessage(msg):
 
 # 主函数
 def main():
-    getCookies()
-    params = getUnSignedTasks()
-    form = getDetailTask(params)
-    submit(form)
+    for user in config['users']:
+        apis = getCpdailyApis(user)
+        session = getSession(user, apis)
+        params = getUnSignedTasks(session, apis)
+        task = getDetailTask(session, params, apis)
+        form = fillForm(task, session, user, apis)
+        # form = getDetailTask(session, user, params, apis)
+        submitForm(session, user, form, apis)
 
 
 # 提供给腾讯云函数调用的启动函数
